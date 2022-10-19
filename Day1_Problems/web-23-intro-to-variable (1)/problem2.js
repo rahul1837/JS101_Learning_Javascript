7c428d622cc5dace61815b8f7a119504bb7ad062
“Rahul <rahulkumar4303411@gmail.com>
2022-10-19 12:35:43 +0530

    Day1 Programs

---
+++ Day1_Problems/web-23-intro-to-variable (1)/problem2.js
@@ -0,0 +1,7 @@
+let name ="Rahul";
+console.log(name);
+name = "Rajaram Pandey";
+console.log(name);
+name = "Baby Devi";
+console.log(name);
+
---
+++ Day1_Problems/web-23-intro-to-variable (1)/problem4.js
@@ -0,0 +1,20 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+
+
+console.log("name = Rahul")
+
+console.log("School = Masai school")
+
+console.log("Grade = 12");
+
+console.log("Section = A1")
+
+console.log("Rollno = 15")
+
+console.log("english = 75")
+
+console.log("maths = 75")
+
+console.log("physics = 89");
+
+console.log("🇷​​​​​🇪​​​​​🇵​​​​​🇴​​​​​🇷​​​​​🇹​​​​​ 🇨​​​​​🇦​​​​​🇷​​​​​🇩​​​​​");
